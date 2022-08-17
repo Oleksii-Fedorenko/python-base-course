107b118ee9c2e6edfe8ae9f25d0779008a57ce10
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-08-17 18:23:45 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+first_name = 'Oleksii'
+last_name = 'Fedorenko'
+full_name = first_name + ' ' + last_name
+print("Hello", full_name)
+# print(type(name))
+
+age = 31
+age += 1
+print(f'Your age is: {age}')
+# print(type(age))
+
+height = 250.5
+print('Your height is: ' + str(height) + 'cm')
+# print(type(height))
+
+human = True
+print('You are a human: ' + str(human))
+# print(type(human))
