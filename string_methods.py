04cf6dbf4d5f6aa9fc8525ac336bc8cac5230811
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-08-17 19:10:07 +0300

    learning input variables and string methods

---
+++ string_methods.py
@@ -0,0 +1,38 @@
+# string methods
+
+# name = 'Alex'
+# age = 31
+# attractive = True
+
+# name, age, attractive = 'Alex', 31, True
+
+# print(name)
+# print(age)
+# print(attractive)
+
+
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+# Spongebob = Patrick = Sandy = Squidward = 30
+
+# print(Spongebob)
+# print(Patrick)
+# print(Sandy)
+# print(Squidward)
+
+
+# name = 'Oleksii Fedorenko'
+
+# print(len(name))
+# print(name.find('e'))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count('o'))
+# print(name.replace('o', 'yo'))
+# print(name * 2)
--- variables.py
+++ variables.py
@@ -1,18 +1,32 @@
-first_name = 'Oleksii'
-last_name = 'Fedorenko'
-full_name = first_name + ' ' + last_name
-print("Hello", full_name)
+# first_name = 'Oleksii'
+# last_name = 'Fedorenko'
+# full_name = first_name + ' ' + last_name
+# print("Hello", full_name)
 # print(type(name))
 
-age = 31
-age += 1
-print(f'Your age is: {age}')
+# age = 31
+# age += 1
+# print(f'Your age is: {age}')
 # print(type(age))
 
-height = 250.5
-print('Your height is: ' + str(height) + 'cm')
+# height = 250.5
+# print('Your height is: ' + str(height) + 'cm')
 # print(type(height))
 
-human = True
-print('You are a human: ' + str(human))
+# human = True
+# print('You are a human: ' + str(human))
 # print(type(human))
+
+# type casting = convert the data type of a value to another data type
+
+x = 1     # int
+y = 2.0   # float
+z = "3"   # str
+
+# x = str(x)
+# y = str(y)
+# z = str(z)
+
+print("X is " + str(x))
+print("Y is " + str(y))
+print(z * 3)
