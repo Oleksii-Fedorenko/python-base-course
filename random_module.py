af31bf91b5cc86a184f4f110fe445de98c2f5f95
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-08-22 19:25:19 +0300

    learning exceptions and random module

---
+++ random_module.py
@@ -0,0 +1,35 @@
+# import random
+
+# x = random.randint(1, 6)
+# y = random.random()
+
+# my_list = ['rock', 'paper', 'scissors']
+# z = random.choice(my_list)
+# cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
+
+# random.shuffle(cards)
+# print(cards)
+
+# ------------------------------------------------
+
+# exception = events detected during execution that interrupt the flow of program
+
+try:
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator / denominator
+
+except ZeroDivisionError as e:
+    print("You can't divide by zero! idiot!")
+    print(e)
+except ValueError as e:
+    print("Enter only numbers plz!")
+    print(e)
+
+except Exception as e:
+    print("error man! need to fix this")
+    print(e)
+else:
+    print(result)
+finally:
+    print("This will asways execute")
