06917669d41ffa08de8fd9c0e21c04154564c443
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-08-23 15:29:47 +0300

    learning modules and imports

---
+++ module/hello_youtube.py
@@ -0,0 +1,8 @@
+# module = a file containing python code. May contain functions, classes, etc. 
+
+from sys import modules
+from messages import hello, bye
+hello()
+bye()
+
+#help('modules') # all modules what you can import
